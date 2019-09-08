38cc14464ed93fa3879db28c9a293e1183157068
Eakkawitch <jinotuto@gmail.com>
2019-09-08 12:36:04 +0700

    put

---
+++ app/src/main/java/com/e/midterm07580487/login.java
@@ -0,0 +1,61 @@
+package com.e.midterm07580487;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+    Button loginButton = findViewById(R.id.button);
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String[] id = {"aaa", "bbb"};
+                String[] password = {"111", "222"};
+                EditText emailEditText = findViewById(R.id.email_edit_text);
+                EditText passwordEditText = findViewById(R.id.password_edit_text);
+                Handler handler = new Handler();
+                Intent intent = new Intent(emailEditText.getContext(), profile.class);
+
+                //    intent.putExtra(key, value); ⇐ แทรกบรรทัดนี้ (key/value เป็นค่าสตริง)
+
+                boolean x = false;
+                for (int i = 0; i < id.length; i++) {
+                    if (emailEditText.getText().toString().equals(id[i]) && passwordEditText.getText().toString().equals(password[i]))
+                        x = true;
+
+                }
+
+                if (x) {
+                    startActivity(intent);
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            //ทำไรก็ได้ในช่วงดีเล
+                            Intent intent = new Intent(login.this, profile.class);//เรียกหน้า
+                            startActivity(intent);
+                            Toast.makeText(login.this, "Hello", Toast.LENGTH_SHORT).show();
+                            finish();
+                        }
+                    }, 2000);
+                }
+               else
+                    Toast.makeText(login.this, "Hello", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/e/midterm07580487/profile.java
@@ -0,0 +1,15 @@
+package com.e.midterm07580487;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+    }
+}
